83cca9e0ec69f19aa8df2a12a1c5663dbaba7281
tonykobe8 <tonykobe8@gmail.com>
2020-08-22 13:12:03 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -6,7 +6,18 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 let nam =[
-  {sname:'kobe',nam:'tony',Age:40,sGrd:'grade1'}, {sname:'maifo',nam:'matome',Age:30,grade:'grade1'}];
+  {sname:'kobe',nam:'tony',Age:40,sGrd:'grade1'}, 
+  {sname:'malete',nam:'dimpo',Age:39,sGrd:'grade2'},
+ {sname:'masha',nam:'masego',Age:32,grade:'grade4'},
+{sname:'mohlapi',name:'racheal',Age:35,sGrd:'grade3'}
+,
+{snam:'aemo',name:'raisibe',Age:31,sGrd:'grade8'},
+
+
+  {sname:'moremi',name:'mpho',Age:32,grade:'grade6'},
+  {sname:'malema',name:'simon',Age:29,sGrd:'grade10'},
+  
+  {sname:'maifo',nam:'matome',Age:30,grade:'grade1'}];
   function display (nam) {
     if(nam > 45){
       return (nam = 'oldest');
@@ -15,5 +26,4 @@ let nam =[
     }
      }
 console.log (nam[0].sname +" "+
-nam[0].nam + " "+
-nam[0].Age ,nam);
+nam[0].nam );
